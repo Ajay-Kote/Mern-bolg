2d87dc0bac25358d03778b75ab595dfd24d86080
Ajay Kote <akajaykote@gmail.com>
2025-09-20 21:26:01 +0530

    added render fontend url server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-frontend-domain.vercel.app'] 
+    ? ['https://blog-write-nti8.onrender.com/'] 
     : ['http://localhost:3000'],
   credentials: true
 }));
